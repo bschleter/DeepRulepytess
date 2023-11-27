a990e67580557f6c1778fa581df4cb7909b3527d
bschleter <42521730+bschleter@users.noreply.github.com>
2023-11-27 11:51:56 -0500

    Update ocr.py
    changes to path and original ocr.py code in the image_path function. changed the path to match a google drive path.

--- ocr.py
+++ ocr.py
@@ -28,43 +28,34 @@ def ocr_space_file(filename, overlay=False, api_key='e61f4d4c3488957', language=
                           )
     return r.content.decode()
 def ocr_result(image_path):
-    subscription_key = "ad143190288d40b79483aa0d5c532724"
-    vision_base_url = "https://westus2.api.cognitive.microsoft.com/vision/v2.0/"
-    ocr_url = vision_base_url + "recognizeText?mode=Printed"
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key, 'Content-Type': 'application/octet-stream'}
-    params = {'language': 'eng', 'detectOrientation': 'true'}
-
     image = Image.open(image_path)
     enh_con = ImageEnhance.Contrast(image)
     contrast = 2.0
     image = enh_con.enhance(contrast)
-    # image = image.convert('L')
-    # image = image.resize((800, 800))
-    image.save('OCR_temp.png')
-    image_data = open('OCR_temp.png', "rb").read()
-    response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
-    response.raise_for_status()
-    op_location = response.headers['Operation-Location']
-    analysis = {}
-    while "recognitionResults" not in analysis.keys():
-        time.sleep(3)
-        binary_content = requests.get(op_location, headers=headers, params=params).content
-        analysis = json.loads(binary_content.decode('ascii'))
-    line_infos = [region["lines"] for region in analysis["recognitionResults"]]
+
+    # Perform OCR using pytesseract
+    text = pytesseract.image_to_string(image, lang='eng')
+
+    # Process the text to extract word bounding boxes
     word_infos = []
-    for line in line_infos:
-        for word_metadata in line:
-            for word_info in word_metadata["words"]:
-                if 'confidence' in word_info.keys():
-                    if word_info['confidence'] == 'Low':
-                        continue
-                if word_info['boundingBox'][0] > word_info['boundingBox'][4]:
-                    continue
-                word_infos.append(word_info)
+    for line in text.split('\n'):
+        words = line.split()
+        for word in words:
+            if not word:
+                continue
+            # Assuming that word_info["boundingBox"] should be in the format [left, top, right, bottom]
+            bounding_box = [0, 0, 0, 0]
+            word_info = {
+                "text": word,
+                "boundingBox": bounding_box
+            }
+            word_infos.append(word_info)
+
     return word_infos
-image_path = 'C:\\work\\evalset_fqa\\vbar\\bitmap\\'
+#usage with whatever Path-example in google drive 
+image_path = '/content/drive/MyDrive/evalset_fqa/vbar/bitmap'
 image_names = os.listdir(image_path)
-for name in ['495.jpg', '151,jpg']:
+for name in ['495.jpg', '151.jpg']:
     image_file_path = os.path.join(image_path, name)
     result = ocr_result(image_file_path)
     print(result)
